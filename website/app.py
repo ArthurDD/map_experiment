bb7ad296cca264ffb4d1a2ecd16cf4491c6b9d17
Arthur <apd321@ic.ac.uk>
2022-11-16 02:41:41 +0000

    Added session system

--- website/app.py
+++ website/app.py
@@ -1,11 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 
 app = Flask(__name__)
+
+# Details on the Secret Key: https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY
+# NOTE: The secret key is used to cryptographically-sign the cookies used for storing
+#       the session data.
+app.secret_key = 'BAD_SECRET_KEY'
 
 
 @app.route('/')
 def home():
     return render_template('base.html')
+
+
+@app.route('/set_name', methods=["GET", "POST"])
+def set_name():
+    if request.method == "POST":
+        session['name'] = request.form['name']
+
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
